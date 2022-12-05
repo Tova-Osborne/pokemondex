a86322f02dc0283217f01ba679d27350d2389249
Tova Osborne <tovaoz@gmail.com>
2022-12-05 14:50:06 -0800

    1.3 For loop
    for loop with conditionals

--- js/scripts.js
+++ js/scripts.js
@@ -1,65 +1,35 @@
 let pokemonList = [{
     name: "Bulbasaur",
     height: 2.04,
-    types: ["Grass", "Poison"]
+    types: [" Grass", " Poison"]
   },
   {
     name: "Charizard",
     height: 5.07,
-    types: ["Fire", "Flying"]
+    types: [" Fire", " Flying"]
   },
   {
     name: "Butterfree",
     height: 3.07,
-    types: ["Bug", "Flying"]
+    types: [" Bug", " Flying"]
   }
 ];
 
 
-/*for (let i = 0; i < artList.length; i++) {
-  if (artList[i].period < 1900) {
-    document.write(`Paintings from the 19th Century:
-      ${artList[i].title}
-      ${artList[i].artistDisplayName}
-      ${artList[i].period}`)
-  } else if (artList[i].period > 1900 && artList[i].period < 2000) {
-    document.write(`Paintings from the 20th Century:
-      ${artList[i].title}
-      ${artList[i].artistDisplayName}
-      ${artList[i].period}`)
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height < 2.5) {
+    document.write(`
+      Name: ${pokemonList[i].name}
+      Height: ${pokemonList[i].height} - Wow, that's a small one!
+      Types: ${pokemonList[i].types}`)
+  } else if (pokemonList[i].height > 5) {
+    document.write(`
+      Name: ${pokemonList[i].name}
+      Height: ${pokemonList[i].height} - Wow, that's a big one!
+      Types: ${pokemonList[i].types}`)
   } else
-    document.write(`Paintings from the 21st Century:
-      ${artList[i].title}
-      ${artList[i].artistDisplayName}
-      ${artList[i].period}`)
+    document.write(`
+      Name: ${pokemonList[i].name}
+      Height: ${pokemonList[i].height}
+      Types: ${pokemonList[i].types}`)
 }
-
-
-
-
-/*OLD ARRAY
-
-let painting1 = {
-  title:"Mother and Child",
-  artistDisplayName:"Njideka Akunyili Crosby",
-  period:2011,
-  contempories: ["Thomas Houseago", "Mark Bradford", "Sigmar Polke"]
-  //primaryImage:
-};
-let painting2 = {
-  title:"Interaction of color",
-  artistDisplayName:"Josef Albers",
-  period:1963,
-  contempories: ["Walter Gropius", "Ludwig Mies Van Der Rohe", "Josef Matthias Hauer"]
-  //primaryImage:
-};
-let painting3 = {
-  title:"Repose",
-  artistDisplayName:"John White Alexander",
-  period:1895,
-  contempories: ["Thomas Wilmer Dewing", "Cecilia Beaux", "Thomas Birch"]
-  //primaryImage:
-};
-
-let artList = [painting1, painting2, painting3];
-*/
