ba7a3a4f34e0cb529ca48282927ca24f184cf272
Tova Osborne <tovaoz@gmail.com>
2022-12-05 14:39:43 -0800

    Task 1.2

---
+++ js/scripts.js
@@ -0,0 +1,65 @@
+let pokemonList = [{
+    name: "Bulbasaur",
+    height: 2.04,
+    types: ["Grass", "Poison"]
+  },
+  {
+    name: "Charizard",
+    height: 5.07,
+    types: ["Fire", "Flying"]
+  },
+  {
+    name: "Butterfree",
+    height: 3.07,
+    types: ["Bug", "Flying"]
+  }
+];
+
+
+/*for (let i = 0; i < artList.length; i++) {
+  if (artList[i].period < 1900) {
+    document.write(`Paintings from the 19th Century:
+      ${artList[i].title}
+      ${artList[i].artistDisplayName}
+      ${artList[i].period}`)
+  } else if (artList[i].period > 1900 && artList[i].period < 2000) {
+    document.write(`Paintings from the 20th Century:
+      ${artList[i].title}
+      ${artList[i].artistDisplayName}
+      ${artList[i].period}`)
+  } else
+    document.write(`Paintings from the 21st Century:
+      ${artList[i].title}
+      ${artList[i].artistDisplayName}
+      ${artList[i].period}`)
+}
+
+
+
+
+/*OLD ARRAY
+
+let painting1 = {
+  title:"Mother and Child",
+  artistDisplayName:"Njideka Akunyili Crosby",
+  period:2011,
+  contempories: ["Thomas Houseago", "Mark Bradford", "Sigmar Polke"]
+  //primaryImage:
+};
+let painting2 = {
+  title:"Interaction of color",
+  artistDisplayName:"Josef Albers",
+  period:1963,
+  contempories: ["Walter Gropius", "Ludwig Mies Van Der Rohe", "Josef Matthias Hauer"]
+  //primaryImage:
+};
+let painting3 = {
+  title:"Repose",
+  artistDisplayName:"John White Alexander",
+  period:1895,
+  contempories: ["Thomas Wilmer Dewing", "Cecilia Beaux", "Thomas Birch"]
+  //primaryImage:
+};
+
+let artList = [painting1, painting2, painting3];
+*/
