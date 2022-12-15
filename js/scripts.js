e86ec5dee47ed4fac7d3aff3dbacf349a11f5562
Tova Osborne <tovaoz@gmail.com>
2022-12-15 11:52:56 -0800

    Created IIFE
    Created the IIFE array/function and update the foreach loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,53 @@
-let pokemonList = [{
-    name: "Bulbasaur",
-    height: 2.04,
-    types: [" Grass", " Poison"]
-  },
-  {
-    name: "Charizard",
-    height: 5.07,
-    types: [" Fire", " Flying"]
-  },
-  {
-    name: "Butterfree",
-    height: 3.07,
-    types: [" Bug", " Flying"]
-  }
-];
+let pokemonRepository = (function() {
 
-pokemonList.forEach(function(pokemon) {
-  document.write (`
-    Name: ${pokemon.name}
-    Height: ${pokemon.height}
-    Types: ${pokemon.types}`);
-});
+  let pokemonList = [{
+      name: "Bulbasaur",
+      height: 2.04,
+      types: [" Grass", " Poison"]
+    },
+    {
+      name: "Charizard",
+      height: 5.07,
+      types: [" Fire", " Flying"]
+    },
+    {
+      name: "Butterfree",
+      height: 3.07,
+      types: [" Bug", " Flying"]
+    }
+  ];
+//function to add new pokemon to the pokemonList array via push (upend the existing array)
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+//getAll function to return all of the items in the pokemonList array
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
+pokemonRepository.getAll.forEach(){
+  if (pokemonRepository.getAll.height < 2.5) {
+  document.write(`
+    Name: ${pokemonRepository.getAll.name}
+    Height: ${pokemonRepository.getAll.height} - Wow, that's a small one!
+    Types: ${pokemonRepository.getAll.types}`)
+} else if (pokemonRepository.getAll.height > 5) {
+  document.write(`
+    Name: ${pokemonRepository.getAll.name}
+    Height: ${pokemonRepository.getAll.height} - Wow, that's a big one!
+    Types: ${pokemonRepository.getAll.types}`)
+} else
+  document.write(`
+    Name: ${pokemonRepository.getAll.name}
+    Height: ${pokemonRepository.getAll.height}
+    Types: ${pokemonRepository.getAll.types}`)
+}
 
 // pokemonList.forEach((name, height, types) => {
 //   document.write(`
