bc9adf7c099ec4b07f832f34d75af839425dd343
Tova Osborne <tovaoz@gmail.com>
2022-12-16 14:08:27 -0800

    Task 1.6 DOM Interaction

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 let pokemonRepository = (function() {
-
+  //pokemon array
   let pokemonList = [{
       name: "Bulbasaur",
       height: 2.04,
@@ -16,16 +16,30 @@ let pokemonRepository = (function() {
       types: [" Bug", " Flying"]
     }
   ];
+
   //function to add new pokemon to the pokemonList array via push (upend the existing array). Check that the input is an object.
   function add(pokemon) {
-    if (typeof pokemon != "object") {
-      console.log("Please enter a valid Pokemon");
-    } else {
+    if (typeof pokemon === "object" && 
+    Object.keys(pokemon).includes("name") && 
+    Object.keys(pokemon).includes("height") && 
+    Object.keys(pokemon).includes("types")) {
       pokemonList.push(pokemon);
+     } else {
+      alert("Please fill in all fields to add a Pokemon");
     }
   }
-  //check that each object.keys matches the format
-  Object.keys(pokemonList).forEach(item => console.log(pokemonList[item]));
+
+  //Add pokemon to the list with the format of a button 
+  function addListItem(pokemon){
+    let pokemonList = document.querySelector('.pokemon-list');
+    let pokemonItem = document.createElement('li');
+    let button = document.createElement('button');
+    button.innerText = pokemon.name; 
+    button.classList.add('name-button');
+    button.addEventListener('click', showDetails => console.log(pokemon));
+    pokemonItem.appendChild(button);
+    pokemonList.appendChild(pokemonItem);
+  }
 
   //getAll function to return all of the items in the pokemonList array
   function getAll() {
@@ -34,25 +48,13 @@ let pokemonRepository = (function() {
 
   return {
     add: add,
-    getAll: getAll
+    getAll: getAll,
+    addListItem: addListItem
   };
+
 })();
 
 //go to the pokemonRepository variable which should return the pokemon list via .gitAll*key is it's a funciton needs () and perform a forEach loop through each parameter of the pokemonRepo
 pokemonRepository.getAll().forEach(function(pokemon) {
-  if (pokemon.height < 2.5) {
-    document.write(`
-    Name: ${pokemon.name}
-    Height: ${pokemon.height} - Wow, that's a small one!
-    Types: ${pokemon.types}`)
-  } else if (pokemon.height > 5) {
-    document.write(`
-    Name: ${pokemon.name}
-    Height: ${pokemon.height} - Wow, that's a big one!
-    Types: ${pokemon.types}`)
-  } else
-    document.write(`
-    Name: ${pokemon.name}
-    Height: ${pokemon.height}
-    Types: ${pokemon.types}`)
+  pokemonRepository.addListItem(pokemon);
 });
