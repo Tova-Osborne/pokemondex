4f0c9ec81e6d7fd06f5a379739bb6e31faa8dcf5
Tova Osborne <tovaoz@gmail.com>
2022-12-20 12:45:07 -0800

    Task 1.7

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,14 @@
 let pokemonRepository = (function() {
   //pokemon array
-  let pokemonList = [{
-      name: "Bulbasaur",
-      height: 2.04,
-      types: [" Grass", " Poison"]
-    },
-    {
-      name: "Charizard",
-      height: 5.07,
-      types: [" Fire", " Flying"]
-    },
-    {
-      name: "Butterfree",
-      height: 3.07,
-      types: [" Bug", " Flying"]
-    }
-  ];
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
   //function to add new pokemon to the pokemonList array via push (upend the existing array). Check that the input is an object.
   function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+  
+  /*function add(pokemon) {
     if (typeof pokemon === "object" && 
     Object.keys(pokemon).includes("name") && 
     Object.keys(pokemon).includes("height") && 
@@ -27,7 +17,7 @@ let pokemonRepository = (function() {
      } else {
       alert("Please fill in all fields to add a Pokemon");
     }
-  }
+  }*/
 
   //Add pokemon to the list with the format of a button 
   function addListItem(pokemon){
@@ -36,13 +26,9 @@ let pokemonRepository = (function() {
     let button = document.createElement('button');
     button.innerText = pokemon.name; 
     button.classList.add('name-button');
-    button.addEventListener('click', (Event) => showDetails(pokemon));
+    button.addEventListener('click', Event => showDetails(pokemon));
     pokemonItem.appendChild(button);
     pokemonList.appendChild(pokemonItem);
-  }
-
-  function showDetails(pokemon){
-    console.log(pokemon);
   }
 
   //getAll function to return all of the items in the pokemonList array
@@ -50,15 +36,53 @@ let pokemonRepository = (function() {
     return pokemonList;
   }
 
+  function loadList() {
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json){
+      json.results.forEach(function (item){
+        let pokemon = {
+          name: item.name,
+          deatilsUrl: item.url
+        };
+        add(pokemon);
+      });
+    }).catch(function (e) {
+      console.error(e);
+    })
+  }
+
+  function loadDetails(item) {
+    let url = item.detailsUrl;
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      item.imageURL = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+    }).catch(function (e) {
+      console.log(e);
+    });
+  }
+
+  function showDetails(pokemon){
+    loadDetails(pokemon).then(function() {
+      console.log(pokemon);
+    }); 
+  }
+
   return {
     add: add,
     getAll: getAll,
-    addListItem: addListItem
+    addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails
   };
 
 })();
-
+pokemonRepository.loadList().then(function(){
 //go to the pokemonRepository variable which should return the pokemon list via .gitAll*key is it's a funciton needs () and perform a forEach loop through each parameter of the pokemonRepo
 pokemonRepository.getAll().forEach(function(pokemon) {
   pokemonRepository.addListItem(pokemon);
+});
 });
