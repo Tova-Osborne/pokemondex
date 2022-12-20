e352b43624e0b65cd300b4170a45143cf6b9283a
Tova Osborne <tovaoz@gmail.com>
2022-12-20 14:18:50 -0800

    added comments

--- js/scripts.js
+++ js/scripts.js
@@ -9,12 +9,12 @@ let pokemonRepository = (function() {
   }
 
 
-  //Add pokemon to the list with the format of a button 
-  function addListItem(pokemon){
+  //Add pokemon to the list with the format of a button
+  function addListItem(pokemon) {
     let pokemonList = document.querySelector('.pokemon-list');
     let pokemonItem = document.createElement('li');
     let button = document.createElement('button');
-    button.innerText = pokemon.name; 
+    button.innerText = pokemon.name;
     button.classList.add('name-button');
     button.addEventListener('click', Event => showDetails(pokemon));
     pokemonItem.appendChild(button);
@@ -26,39 +26,45 @@ let pokemonRepository = (function() {
     return pokemonList;
   }
 
+  //loadList function gets the pokemon list from the pokeAPI 
   function loadList() {
-    return fetch(apiUrl).then(function (response) {
+    return fetch(apiUrl).then(function(response) {
       return response.json();
-    }).then(function (json){
-      json.results.forEach(function (item){
+    }).then(function(json) {
+      //forEach loop to get the name and detail URL from the pokeAPI response
+      json.results.forEach(function(item) {
         let pokemon = {
           name: item.name,
           detailsUrl: item.url
         };
+        //calls the add function to add the "items"/pokemon to the pokemonList array 
         add(pokemon);
       });
-    }).catch(function (e) {
+      //if error log to the console 
+    }).catch(function(e) {
       console.error(e);
     })
   }
 
+  //loads the details of the pokemon from the detailsUrl call 
   function loadDetails(item) {
     let url = item.detailsUrl;
-    return fetch(url).then(function (response) {
+    return fetch(url).then(function(response) {
       return response.json();
-    }).then(function (details) {
+      //from the details API get the image, height, and types associated with each pokemon
+    }).then(function(details) {
       item.imageURL = details.sprites.front_default;
       item.height = details.height;
       item.types = details.types;
-    }).catch(function (e) {
+    }).catch(function(e) {
       console.log(e);
     });
   }
 
-  function showDetails(pokemon){
+  function showDetails(pokemon) {
     loadDetails(pokemon).then(function() {
       console.log(pokemon);
-    }); 
+    });
   }
 
   return {
@@ -70,9 +76,9 @@ let pokemonRepository = (function() {
   };
 
 })();
-pokemonRepository.loadList().then(function(){
-//go to the pokemonRepository variable which should return the pokemon list via .gitAll*key is it's a funciton needs () and perform a forEach loop through each parameter of the pokemonRepo
-pokemonRepository.getAll().forEach(function(pokemon) {
-  pokemonRepository.addListItem(pokemon);
-});
+pokemonRepository.loadList().then(function() {
+  //go to the pokemonRepository variable which should return the pokemon list via .gitAll*key is it's a funciton needs () and perform a forEach loop through each parameter of the pokemonRepo
+  pokemonRepository.getAll().forEach(function(pokemon) {
+    pokemonRepository.addListItem(pokemon);
+  });
 });
